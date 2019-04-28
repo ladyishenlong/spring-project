4f0b7d26d81cc9adfa747527339e40ab8e7eef7c
ladyishenlong <30828481+ladyishenlong@users.noreply.github.com>
2019-04-28 15:27:00 +0800

    redis的配置以及使用

--- spring-project-redis/src/main/java/com/ladyishenlong/springprojectredis/controller/RedisController.java
+++ spring-project-redis/src/main/java/com/ladyishenlong/springprojectredis/controller/RedisController.java
@@ -1,12 +1,37 @@
 package com.ladyishenlong.springprojectredis.controller;
 
+import com.ladyishenlong.springprojectredis.service.RedisService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-
+/**
+ * 参考 https://www.cnblogs.com/garfieldcgf/p/6510762.html
+ */
+@RestController
+@RequestMapping("/redis")
 public class RedisController {
 
+    @Autowired
+    RedisService redisService;
 
+    @GetMapping("/helloRedis")
+    public String helloRedis() {
+        return "hello redis";
+    }
+
+    @GetMapping("/setRedis")
+    public void setRedis() {
+        redisService.setValue("key", "hello redis");
+    }
+
+    @GetMapping("/getRedis")
+    public String getRedis() {
+        return "从redis获得key的值是：" + redisService.getValue("key");
+    }
 
 
 }

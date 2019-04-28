66cdd304f29d696f74334b4555f67439be0f0c0c
ladyishenlong <30828481+ladyishenlong@users.noreply.github.com>
2019-04-28 16:05:21 +0800

    feign 调用远程服务

---
+++ spring-boot-feign/src/test/java/com/ladyishenlong/springbootfeign/SpringBootFeignApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ladyishenlong.springbootfeign;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootFeignApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

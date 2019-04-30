502449bf9826c2d29450484236727a637a0a84cc
ladyishenlong <30828481+ladyishenlong@users.noreply.github.com>
2019-04-30 09:16:57 +0800

    springboot学习

--- src/main/java/com/ladyishenlong/springproject/SpringProjectApplication.java
+++ src/main/java/com/ladyishenlong/springproject/SpringProjectApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+
+/**
+ * todo 还需要了解下集群 ;文件的上传下载
+ */
 @EnableEurekaServer
 @SpringBootApplication
 public class SpringProjectApplication {
